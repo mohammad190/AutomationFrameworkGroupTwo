552d1507be6e797c054d28396dd03ec8653c5a14
Raheekhan <ibrahimkhan0894@gmail.com>
2017-09-19 14:19:11 -0400

    Added reader

---
+++ Generic/src/main/java/utility/DataReader.java
@@ -0,0 +1,80 @@
+package utility;
+
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.xssf.usermodel.XSSFCell;
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+public class DataReader {
+
+    XSSFWorkbook workbook = null;
+    XSSFSheet sheet = null;
+    Cell cell = null;
+    FileOutputStream fio = null;
+    int numberOfRows, numberOfCol, rowNum;
+
+    public String[][] fileReader1(String path) throws IOException {
+        String[][] data = {};
+        File file = new File(path);
+        FileInputStream fis = new FileInputStream(file);
+        workbook = new XSSFWorkbook(fis);
+        sheet = workbook.getSheetAt(0);
+        numberOfRows = sheet.getLastRowNum();
+        numberOfCol = sheet.getRow(0).getLastCellNum();
+        data = new String[numberOfRows + 1][numberOfCol + 1];
+
+        for(int i = 1; i < data.length; i++) {
+            XSSFRow rows = sheet.getRow(i);
+            for(int j = 0; j < numberOfCol; j++) {
+                XSSFCell cell = rows.getCell(j);
+                String cellData = getCellValue(cell);
+                data[i][j] = cellData;
+            }
+        }
+        return data;
+    }
+
+    public  String[] fileReader(String path) throws IOException {
+        String[] data = {};
+        File file = new File(path);
+        FileInputStream fis = new FileInputStream(file);
+        workbook = new XSSFWorkbook(fis);
+        sheet = workbook.getSheetAt(0);
+        numberOfRows = sheet.getLastRowNum();
+        numberOfCol = sheet.getRow(0).getLastCellNum();
+        data = new String[numberOfRows + 1];
+
+        for(int i = 1; i < data.length; i++) {
+            XSSFRow rows = sheet.getRow(i);
+            for(int j = 0; j < numberOfCol; j++) {
+                String cellData = getCellValue(cell);
+                data[i] = cellData;
+            }
+        }
+        return data;
+    }
+
+    public String getCellValue(XSSFCell cell) {
+        Object value = null;
+
+        int dataType = cell.getCellType();
+        switch (dataType) {
+            case XSSFCell.CELL_TYPE_NUMERIC:
+                value = cell.getNumericCellValue();
+                break;
+            case XSSFCell.CELL_TYPE_STRING:
+                value = cell.getStringCellValue();
+                break;
+            case XSSFCell.CELL_TYPE_BOOLEAN:
+                value = cell.getBooleanCellValue();
+                break;
+        }
+        return value.toString();
+    }
+}
