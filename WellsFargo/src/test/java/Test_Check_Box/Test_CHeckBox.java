dfb302a116009797a23e153a191e64714c0ae754
ali <sujonpueee@gmail.com>
2017-09-21 03:19:34 -0400

    Add some testCases

---
+++ WellsFargo/src/test/java/Test_Check_Box/Test_CHeckBox.java
@@ -0,0 +1,16 @@
+package Test_Check_Box;
+
+
+import Check_Box.Atm_Loaction_check_Box;
+import base.CommonAPI;
+import org.openqa.selenium.support.PageFactory;
+import org.testng.annotations.Test;
+
+public class Test_CHeckBox extends CommonAPI {
+
+    @Test
+    public void Check_Box_example()throws InterruptedException{
+        Atm_Loaction_check_Box Atm = PageFactory.initElements(driver, Atm_Loaction_check_Box.class);
+        Atm.Check_Box_Option();
+    }
+}
