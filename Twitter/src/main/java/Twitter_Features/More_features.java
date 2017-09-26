00bd432a989a106f5f05439b6eb0c8e4e911d476
ali <sujonpueee@gmail.com>
2017-09-26 18:08:01 -0400

    Twitter Incomplete

--- Twitter/src/main/java/Twitter_Features/More_features.java
+++ Twitter/src/main/java/Twitter_Features/More_features.java
@@ -4,18 +4,20 @@ import base.CommonAPI;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
-import org.openqa.selenium.support.ui.Select;
 
 public class More_features extends CommonAPI {
 
     @FindBy(how = How.XPATH,using = ".//*[@id='doc']/div[1]/div/div[2]/div/div[2]/span")
     public static WebElement More;
 
+    @FindBy(how = How.XPATH,using = ".//span[@class='StreamsCategoryBar-moreIcon Icon Icon--caretDown']")
+    public static WebElement dropdown;
+
+//    @FindBy(how = How.XPATH,using = ".//span[@class='StreamsCategoryBar-moreIcon Icon Icon--caretDown']")
+//    public static List<WebElement> dropDownOption;
+
     @FindBy(how = How.LINK_TEXT,using = "Arts & Culture")
     public static WebElement Click_Arts_culture_2;
-
-    @FindBy(how = How.XPATH,using = ".//div[@class='dropdown-menu StreamsCategoryBar-dropdownMenu']")
-    public static WebElement dropdown;
 
     @FindBy(how = How.LINK_TEXT,using = "Government & Politics")
     public static WebElement Click_Goverment_politics_3;
@@ -35,50 +37,22 @@ public class More_features extends CommonAPI {
     @FindBy(how = How.LINK_TEXT,using = "Technology")
     public static WebElement Click_Technology_8;
 
-    public static void More_option_features()throws InterruptedException{
+    public void More_option_features()throws InterruptedException{
         More.click();
-        //driver.navigate().forward();
         Thread.sleep(3000);
-
+        //Select sel = new Select(dropdown);
         dropdown.click();
-
-//
-//        Select artsCulture = new Select(Click_Arts_culture_2);
-//        Select goverment_Politics = new Select(Click_Goverment_politics_3);
-//        Select Gaming = new Select(Click_Gaming_4);
-//        Select NonProfit = new Select(Click_Nonprofit_5);
-//        Select Fun = new Select(Click_fun_6);
-//        Select Science = new Select(Click_Science_7);
-//        Select Technology = new Select(Click_Technology_8);
-
-
         Click_Arts_culture_2.click();
-       //driver.navigate().back();
-        Thread.sleep(3000);
 
-        Click_Goverment_politics_3.click();
-        driver.navigate().back();
         Thread.sleep(3000);
-
-        Click_Gaming_4.click();
-        //driver.navigate().back();
-        Thread.sleep(3000);
-
-        Click_Nonprofit_5.click();
-        //driver.navigate().back();
-        Thread.sleep(3000);
-
-        Click_fun_6.click();
-        //driver.navigate().back();
-        Thread.sleep(3000);
-
-        Click_Science_7.click();
-        //driver.navigate().back();
-        Thread.sleep(3000);
-
-        Click_Technology_8.click();
-        //driver.navigate().back();
-        Thread.sleep(3000);
-
+//
+//        for(int i = 0; i<dropDownOption.size();i++){
+//            dropDownOption.get(i).click();
+//            Thread.sleep(3000);
+//        }
     }
+
+//    public void verifyoption()throws InterruptedException{
+//        String actualURL=""
+//    }
 }
