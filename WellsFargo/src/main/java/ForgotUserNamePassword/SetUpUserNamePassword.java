a3d5ad1a05464532ad54b48e4664cfd1d6030969
ali <sujonpueee@gmail.com>
2017-09-28 12:06:00 -0400

    ScreenShots

--- WellsFargo/src/main/java/ForgotUserNamePassword/SetUpUserNamePassword.java
+++ WellsFargo/src/main/java/ForgotUserNamePassword/SetUpUserNamePassword.java
@@ -8,40 +8,50 @@ import org.openqa.selenium.support.How;
 public class SetUpUserNamePassword extends CommonAPI {
 
     @FindBy(how = How.LINK_TEXT,using = "Forgot Password/Username?")
-    public static WebElement Go_UserNamePassword;
+    WebElement Go_UserNamePassword;
 
     @FindBy(how = How.XPATH,using = ".//a[@id='NID1_14_1_1_3_2_3_1']")
-    public static WebElement Go_FindUserName;
+    WebElement Go_FindUserName;
 
     @FindBy(how = How.XPATH,using = ".//input[@id='ssn1']")
-    public static WebElement Go_SSN_Box_1;
+    WebElement Go_SSN_Box_1;
 
     @FindBy(how = How.XPATH,using = ".//input[@id='ssn2']")
-    public static WebElement Go_SSN_Box_2;
+    WebElement Go_SSN_Box_2;
 
     @FindBy(how = How.XPATH,using = ".//input[@id='ssn3']")
-    public static WebElement Go_SSN_Box_3;
+    WebElement Go_SSN_Box_3;
 
     @FindBy(how = How.XPATH,using = ".//input[@id='password']")
-    public static WebElement Go_Password;
+    WebElement Go_Password;
 
     @FindBy(how = How.ID,using = "cont")
-    public static WebElement Go_Continue;
+    WebElement Go_Continue;
 
-    public static void UserNameAndPasswordFeatures(String SSN1, String SSN2, String SSN3, String Password)throws InterruptedException{
+    public void UserNameAndPasswordFeatures(String SSN1, String SSN2, String SSN3, String Password)throws Exception {
         Go_UserNamePassword.click();
-        Thread.sleep(3000);
+        Thread.sleep(500);
         Go_FindUserName.click();
-        Thread.sleep(3000);
+        Thread.sleep(500);
         Go_SSN_Box_1.sendKeys(SSN1);
-        Thread.sleep(3000);
+        Thread.sleep(500);
         Go_SSN_Box_2.sendKeys(SSN2);
-        Thread.sleep(3000);
+        Thread.sleep(500);
         Go_SSN_Box_3.sendKeys(SSN3);
-        Thread.sleep(3000);
+        Thread.sleep(500);
         Go_Password.sendKeys(Password);
-        Thread.sleep(3000);
+        Thread.sleep(500);
         Go_Continue.click();
-        Thread.sleep(3000);
+        Thread.sleep(500);
+
+        takeScreenShot("AfterCaseStatus", "C:\\Users\\sujon\\IdeaProjects\\AutomationFrameworkGroupTwo\\WellsFargo\\screenshots\\");
+        // Try to make the code more clean
+        // Dont use this much of Thread.sleep, one single test case will take too much time otherwise
+        // Try to avoid using Thread.sleep
+        // Just use this screenshot method wherever you need it,
+        // Push and pull, Ive added a feature where it takes screenshot automatically for you
+        // If test case fails
+
+        // Is there aything else you need help with sujon bhai, not now, ok take care bhai
     }
 }
--- WellsFargo/src/main/java/WellsFergo_dropDownMethod/DropDown_Method.java
+++ WellsFargo/src/main/java/WellsFergo_dropDownMethod/DropDown_Method.java
@@ -6,12 +6,14 @@ import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 import org.openqa.selenium.support.ui.Select;
 
+import java.io.IOException;
+
 public class DropDown_Method extends CommonAPI {
 
     @FindBy(how = How.ID,using = "destination")
-    public static WebElement Destination_Box;
+    WebElement Destination_Box;
 
-    public static void DropDownFeatures()throws InterruptedException{
+    public void DropDownFeatures()throws InterruptedException, IOException {
         Destination_Box.click();
         Select select = new Select(Destination_Box);
         select.selectByValue("AccountSummary");
@@ -28,5 +30,15 @@ public class DropDown_Method extends CommonAPI {
         Thread.sleep(2000);
         System.out.println("This all test is passed");
 
+        takeScreenShot("AfterCaseStatus", "C:\\Users\\sujon\\IdeaProjects\\AutomationFrameworkGroupTwo\\WellsFargo\\screenshots\\");
+
+        // Dont need to make WebElement public and static, should work without
+        // It wasnt working because TakeScreenShot ISNT static in CommonAPI but you called it here
+        // inside a static method, thats why. Try now,
+        // And also make sure to push let me know when you pushed then you can pull
+        // Ive added some few more features
+        //
+
     }
+
 }
