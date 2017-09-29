431740fcae47fcd0e3272a3cf71348b837de5bf8
sayem <sayem991@gmail.com>
2017-09-29 13:04:26 -0400

    Version 1.3

--- Amazon/src/main/java/home/TodaysDeals.java
+++ Amazon/src/main/java/home/TodaysDeals.java
@@ -1,16 +1,26 @@
 package home;
 
 import base.CommonAPI;
+import com.relevantcodes.extentreports.LogStatus;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.How;
 
+import java.io.IOException;
+
 public class TodaysDeals extends CommonAPI {
 
-    @FindBy(how = How.XPATH, using = "//div//a[@href='/gp/goldbox/ref=nav_cs_gb/138-7718627-8681933']")
+    @FindBy(how = How.XPATH, using = "html/body/div[1]/header/div/div[2]/div[3]/div/div/a[2]")
     WebElement Todays_Deals;
 
-    public void Todays_Deals() {
+
+
+    public void Todays_Deals() throws IOException {
         Todays_Deals.click();
+        test.log(LogStatus.INFO,"ClickingonTodaysDeals");
+
+        takeScreenShot(driver, "test", "C:\\Users\\SAYEM\\Desktop\\SayemJava\\AutomationFrameworkGroupTwo\\Amazon\\screenshots\\");
+        test.log(LogStatus.FAIL, "Passed Test Case");
+
     }
 }
