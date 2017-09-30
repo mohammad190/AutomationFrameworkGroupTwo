033b75b93f5cf24459b1519e945ab86f326beff1
sayem <sayem991@gmail.com>
2017-09-30 13:15:19 -0400

    Amazon Version 1.2

--- Amazon/src/main/java/home/TodaysDeals.java
+++ Amazon/src/main/java/home/TodaysDeals.java
@@ -13,14 +13,11 @@ public class TodaysDeals extends CommonAPI {
     @FindBy(how = How.XPATH, using = "html/body/div[1]/header/div/div[2]/div[3]/div/div/a[2]")
     WebElement Todays_Deals;
 
-
-
     public void Todays_Deals() throws IOException {
         Todays_Deals.click();
         test.log(LogStatus.INFO,"ClickingonTodaysDeals");
 
         takeScreenShot(driver, "test", "C:\\Users\\SAYEM\\Desktop\\SayemJava\\AutomationFrameworkGroupTwo\\Amazon\\screenshots\\");
         test.log(LogStatus.FAIL, "Passed Test Case");
-
     }
 }
